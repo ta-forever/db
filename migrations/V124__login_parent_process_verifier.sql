19fcf582432b9da5a6fbe9f9a97021614f165874
Axle1975 <loweam@gmail.com>
2025-05-07 23:13:26 +1000

    login and launch verifier tables and per-mod trueskill parameters.

---
+++ migrations/V124__login_parent_process_verifier.sql
@@ -0,0 +1,28 @@
+CREATE TABLE login_verifier (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  login_id MEDIUMINT(8) UNSIGNED NOT NULL,
+  verification_result VARCHAR(255),
+  data TEXT,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (login_id) REFERENCES login(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+ALTER TABLE game_featuredMods
+ADD COLUMN trueskill_env VARCHAR(127) 
+DEFAULT '{"mu":1500, "sigma":500, "beta":240, "tau":10, "draw_probability":0.1}';
+
+CREATE TABLE game_launch_verifier (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  login_id MEDIUMINT(8) UNSIGNED NOT NULL,
+  game_id INT(10) UNSIGNED NOT NULL,
+  data TEXT,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (login_id) REFERENCES login(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
